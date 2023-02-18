059f2fc8aa611b5f21f7bb8e25b3c11e8e72b919
IdzanamiMao <tksalsk@gmail.com>
2023-02-19 01:02:54 +0300

    feat: add pop-up

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
  const button = document.querySelector('.button')
-
-
-
 button.addEventListener ('click', e => {
-  alert ('Hi')
- })
+  win()
+})
+
+const popUp = document.querySelector('.pop-up__wrapper')
+function win() { /*поп-ап выигрыша*/
+  popUp.classList.add("active");
+}
+
+function lost() {/*поп-ап проигрыша*/
+  const img = document.querySelector('.img')
+  const caption = document.querySelector('.pop-up__caption')
+  const desc = document.querySelector('.desc')
+  const button = document.querySelector('.pop-up__button')
+
+  popUp.classList.add("active");
+  popUp.classList.add("lost");
+
+  img.src="./assets/img/poup-lost.png";
+  caption.innerHTML = "Oh, no hay suerte ahora";
+  desc.classList.add("remove");
+  button.innerHTML = "GIRAR DE NUEVO";
+}
+
+const popUpButton = document.querySelector('.pop-up__button')
+popUpButton.addEventListener ('click', e => {
+  popUp.classList.remove("active")
+  popUp.classList.remove("lost");
+})
